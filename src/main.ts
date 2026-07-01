3377562e8a3065fceaa03c4266bebc0c061dffd5
DUNK73 <z7dank3@yandex.ru>
2026-07-01 10:52:00 +0300

    next 8

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
-
   app.enableCors({
-    origin: '*', 
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     preflightContinue: false,
     optionsSuccessStatus: 204,
-    credentials: true, // Если нужно передавать куки/авторизацию
   });
-
-  // Используем middleware cors напрямую
-  app.use(cors({
-    origin: '*',
-    credentials: true,
-  }));
 
   await app.listen(process.env.PORT ?? 3002);
 }
