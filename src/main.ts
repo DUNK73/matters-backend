1433e283eeaf62dca90aa8df0c5c20b5fd45a9ec
DUNK73 <z7dank3@yandex.ru>
2026-07-01 03:36:54 +0300

    next 6

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
+
+  app.enableCors({
+    origin: '*', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true, // Если нужно передавать куки/авторизацию
+  });
+
   await app.listen(process.env.PORT ?? 3002);
 }
 bootstrap();
